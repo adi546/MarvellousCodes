cc89f75cd98dae40956433086ce77f39f5990ad8
adi546 <139097653+adi546@users.noreply.github.com>
2023-12-22 14:18:17 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { DemoComponent } from './demo/demo.component';
+import { FirstCompComponent } from './first-comp/first-comp.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DemoComponent],
+  imports: [CommonModule, RouterOutlet, FirstCompComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MarvellousFirstApp';
+  title = 'Assignment5';
 }
